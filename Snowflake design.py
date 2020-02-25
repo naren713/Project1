f155c2c8eafbacd48d661e5255d0082ed6639dd7
naren713 <57811609+naren713@users.noreply.github.com>
2020-02-25 15:47:44 +0530

    Draws a snowflake design

---
+++ Snowflake design.py
@@ -0,0 +1,29 @@
+import turtle
+
+
+def koch(t, n):
+    if n < 10:
+        t.fd(n)
+        return
+    m = n/3
+    koch(t, m)
+    t.lt(60)
+    koch(t, m)
+    t.rt(120)
+    koch(t, m)
+    t.lt(60)
+    koch(t, m)
+
+
+def snowflake(t, n):
+    for i in range(3):
+        koch(t, n)
+        t.rt(120)
+
+
+bob = turtle.Turtle()
+
+bob.pu()
+bob.goto(-150, 90)
+bob.pd()
+snowflake(bob, 300)
